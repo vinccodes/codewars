2348d579e3e42010171022417baf9dc24d6afbd7
vinccodes <vinccodes@gmail.com>
2021-04-20 23:11:34 -0400

    Initial commit

---
+++ string-challenges/7kyu/numbers-to-letters/index.js
@@ -0,0 +1,13 @@
+function switcher(x){
+    // TODO: convert each item to its unicode 
+    
+    // loop through each element
+    for (let i =0; i < x.length; i ++){
+      // convert element to string 
+      console.log(String.fromCharCode(x[i]));
+      
+      // join the string together
+    }
+    
+    // return as string
+  }
