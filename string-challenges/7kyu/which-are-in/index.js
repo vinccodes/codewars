77fb9f5d534ffc88d0cfa6ccffe161510aa3e1c8
vinccodes <vinccodes@gmail.com>
2021-04-23 15:37:18 -0400

    Added problem as comment.

--- string-challenges/7kyu/which-are-in/index.js
+++ string-challenges/7kyu/which-are-in/index.js
@@ -1,3 +1,9 @@
+/**
+ * Given two arrays of strings a1 and a2
+ * return a sorted array r in lexicographical order
+ * of the strings of a1 which are substrings of strings of a2.
+ */
+
 function inArray(array1,array2){
     
     const result =[]
