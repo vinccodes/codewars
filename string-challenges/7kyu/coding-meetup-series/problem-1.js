7c5353214f0e3cefba25e06bf51c4903f29d284e
vinccodes <vinccodes@gmail.com>
2021-04-24 23:19:25 -0400

    Completed higher order function: filter() problem.

---
+++ string-challenges/7kyu/coding-meetup-series/problem-1.js
@@ -0,0 +1,37 @@
+/*
+You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+Your task is to return the number of JavaScript developers coming from Europe.
+
+For example, given the following list:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+
+your function should return number 1.
+
+If, there are no JavaScript developers from Europe then your function should return 0.
+*/
+
+
+const list1 = [
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+  ];
+
+function countDevelopers(list) {
+    
+    const result = list.filter((devs)=>{
+       if (devs.continent == 'Europe' && devs.language == 'JavaScript') {
+           return devs;
+       }
+    });
+
+    return result;
+}
