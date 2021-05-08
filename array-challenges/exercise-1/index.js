f9f415bf0b3e7517bee83f842ac97cc3f4be90c9
vinccodes <vinccodes@gmail.com>
2021-05-07 22:51:50 -0400

    Added array problem.

---
+++ array-challenges/exercise-1/index.js
@@ -0,0 +1,23 @@
+/*
+What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+addLength('apple ban') => ["apple 5", "ban 3"]
+addLength('you will win') => ["you 3", "will 4", "win 3"]
+
+Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+
+Note: String will have at least one element; words will always be separated by a space.
+
+
+*/
+
+function addLength(str) {
+    // split the string into array of words
+    words = str.split(" ");
+    // map the new word to a new array
+    const result = words.map(item=> {
+        return `${item} ${item.length}`;
+    });
+    return result;
+}
+    
