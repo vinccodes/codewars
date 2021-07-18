288cb997509c3368af0869c72e2fbbc38904a1a6
vinccodes <vinccodes@gmail.com>
2021-07-17 23:13:44 -0400

    Started reverseArr

---
+++ 6kyu/reverseArr.js
@@ -0,0 +1,23 @@
+/*Write a function that will take in any array and reverse it.
+
+Sounds simple doesn't it?
+
+NOTES:
+
+Array should be reversed in place! (no need to return it)
+Usual builtins have been deactivated. Don't count on them.
+You'll have to do it fast enough, so think about performances
+*/
+
+
+// Examples: [1,2,3,4,5] --> [5,4,3,2,1] 0 1 2 3 4
+// Even and Odd Number arrays? 
+function reverse(arr) {
+    // traverse arr using indexing 
+    
+    // pointer to current index and last index
+
+    // swap the elements
+
+    // stopping condition: index of current > index of end
+ }
