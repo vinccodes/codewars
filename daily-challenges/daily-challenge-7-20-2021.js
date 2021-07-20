fc314b8da3731392b5febc7023703a5c76d6982e
vinccodes <vinccodes@gmail.com>
2021-07-20 15:21:52 -0400

    added challenge

---
+++ daily-challenges/daily-challenge-7-20-2021.js
@@ -0,0 +1,13 @@
+
+/* There is an array with some numbers.
+   All numbers are equial except for one.
+    Try to find it
+
+*/
+
+function findUniqueNum(arr) {
+
+}
+
+console.log(findUniqueNum([1,1,1,2,1,1,1]), 2)
+console.log(findUniqueNum([9,9,9,9,0.1,9,9,9,9]), 0.1)
