04a8d2b5015024a31654c1b829d3b4b106d9c59f
vinccodes <vinccodes@gmail.com>
2021-07-22 16:56:04 -0400

    Finished challenge

--- daily-challenges/daily-challenge-7-20-2021.js
+++ daily-challenges/daily-challenge-7-20-2021.js
@@ -5,8 +5,36 @@
 
 */
 
-function findUniqueNum(arr) {
+// What makes a unique number?
+// Only 1 copy, 1 count exist
 
+// Should I use any particular data struct?
+
+
+
+function findUniqueNum(arr) {
+    const numbers = {};
+
+    // Iterate through each element
+    for (let i = 0; i < arr.length; i++) {
+        // if entry already exists, increment one, go to next element
+        if (numbers[arr[i].toString()]) {
+            numbers[arr[i].toString()] = numbers[arr[i].toString()] + 1;
+            continue;
+        }
+        // if the number doesn't exist make an entry
+        numbers[arr[i].toString()] = 1;
+    }   
+    // 2nd pass
+    // go through object and find the one with value of 1 and return that key
+    let foundNum = 0;
+    for (const [key, value] of Object.entries(numbers)) {
+      if (value == 1) {
+        foundNum = Number(key)
+      }
+    }
+    // Return the unique number
+    return foundNum;
 }
 
 console.log(findUniqueNum([1,1,1,2,1,1,1]), 2)
