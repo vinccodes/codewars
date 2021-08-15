ed8959b060b7b27b1f85d74d0802677738b8167a
vinccodes <vinccodes@gmail.com>
2021-08-14 23:49:54 -0400

    Added daily challenge

---
+++ daily-challenges/daily-challenge-8-14-2021.js
@@ -0,0 +1,14 @@
+/*  
+
+Given a string of characters, return the character that appears the most often.
+
+describe("Max Character", () => {
+ it("Should return max character", () => {
+  assert.equal(max("Hello World!"), "l");
+ });
+});
+
+No String or Array Methods (well brute force it first, but then no methods)! 
+
+*/
+
