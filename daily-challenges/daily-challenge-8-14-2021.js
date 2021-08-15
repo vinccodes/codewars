47ff0eafdee45ed58bebcdaa08f227d7cbe159be
vinccodes <vinccodes@gmail.com>
2021-08-15 00:15:07 -0400

    Non optimal solution draft

--- daily-challenges/daily-challenge-8-14-2021.js
+++ daily-challenges/daily-challenge-8-14-2021.js
@@ -12,3 +12,35 @@ No String or Array Methods (well brute force it first, but then no methods)!
 
 */
 
+
+const maxCharacter = (myString)=>{
+    const counter = {};
+    let max = 0;
+    let foundMax = '';
+
+    for (let i = 0; i < myString.length; i++) {
+      // if the value exists, just add 1
+        if (counter[myString[i]]) {
+            counter[myString[i]] += 1;
+
+            // check max value
+            if (counter[myString[i]] > max) {
+              max = counter[myString[i]];
+              foundMax = myString[i];
+            }
+
+            continue;
+        } 
+        // if the value doesn't exist, create it and set its count to 1
+        if (!counter[myString[i]]) {
+          counter[myString[i]] = 1;
+          
+        }   
+    }
+    // console.log(counter);
+    // console.log(foundMax);
+
+    return foundMax;
+};
+
+maxCharacter("impossible")
