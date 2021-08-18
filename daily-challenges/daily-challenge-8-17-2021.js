76010b4caf2b75b0976d730f0122a8834399d64e
vinccodes <vinccodes@gmail.com>
2021-08-17 23:38:22 -0400

    Added reverse items challenge

---
+++ daily-challenges/daily-challenge-8-17-2021.js
@@ -0,0 +1,18 @@
+/* Given an array of items, reverse the order.
+
+No reverse method! Unless that is your brute force first... */
+
+const reverseItems = (items) => {
+
+    // make copy of array 
+    const itemsArr = items;
+    // loop through items array
+    for (let i = 0; i < itemsArr.length; i++) {
+        // does even or odd number of items matter?
+
+        // swap last element with first element
+
+        // increment pointer
+
+    }
+};
